e8859a7d4fa3871c0f1bc9eac209497bba6a1213
bekabot <87282211776dom>
2020-03-16 17:58:02 +0600

    record admin page configured

--- iitu/board/models.py
+++ iitu/board/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 
+RECORD_TYPE_CHOICES = (
+    ('news', 'Новость'),
+    ('ads', 'Объявление'),
+    ('vacancy', 'Вакансия'),
+)
+
+ADS_CATEGORY = (
+    ('sport', 'Спорт'),
+    ('it', 'IT'),
+    ('study', 'Учеба',),
+    ('others', 'Прочее',),
+)
+
 
 class Record(models.Model):
-    record_title = models.TextField(max_length=100, verbose_name="Заголовок", default="")
+    record_title = models.TextField(max_length=200, verbose_name="Заголовок", default="")
     record_body = models.TextField(max_length=10000, verbose_name="Текст", default="")
     image1 = models.ImageField(blank=True, upload_to="images", verbose_name="Картинка1")
     image2 = models.ImageField(blank=True, upload_to="images", verbose_name="Картинка2")
     image3 = models.ImageField(blank=True, upload_to="images", verbose_name="Картинка3")
+    phone = models.CharField(blank=True, max_length=11, verbose_name="Контактный телефон (не обязательно)")
+    email = models.CharField(blank=True, max_length=40, verbose_name="Контактный email (не обязательно)")
+    whatsapp = models.CharField(blank=True, max_length=11, verbose_name="WhatsApp (не обяза тельно)")
+    instagram = models.CharField(blank=True, max_length=30, verbose_name="Instagram логин (не обязательно)")
+    vk = models.CharField(blank=True, max_length=30, verbose_name="VK логин/id (не обязательно)")
+    telegram = models.CharField(blank=True, max_length=30, verbose_name="Telegram логин (не обязательно)")
+    record_type = models.CharField(choices=RECORD_TYPE_CHOICES, max_length=7, default='news', verbose_name="Тип записи")
+    ads_category = models.CharField(blank=True, choices=ADS_CATEGORY, max_length=20,
+                                    verbose_name="Категория (только для новостей)")
 
     def __str__(self):
         return self.record_title
--- iitu/board/serializers.py
+++ iitu/board/serializers.py
@@ -3,15 +3,21 @@ from rest_framework import serializers
 from .models import Record
 
 
+# TODO if null is not accepted by client, try this https://bit.ly/2Wj9gEq
 class RecordSerializer(serializers.Serializer):
-    record_title = serializers.CharField(max_length=100)
+    record_title = serializers.CharField(max_length=200)
     record_body = serializers.CharField(max_length=10000)
-    image1 = serializers.ImageField(max_length=None, use_url=True, allow_null=True,
-                                    required=False)  # todo rename to eng symbols
-    image2 = serializers.ImageField(max_length=None, use_url=True, allow_null=True,
-                                    required=False)  # todo rename to eng symbols
-    image3 = serializers.ImageField(max_length=None, use_url=True, allow_null=True,
-                                    required=False)  # todo rename to eng symbols
+    image1 = serializers.ImageField(max_length=None, use_url=True, allow_empty_file=True, required=False)
+    image2 = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
+    image3 = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
+    phone = serializers.CharField(max_length=11)
+    email = serializers.CharField(max_length=40)
+    whatsapp = serializers.CharField(max_length=11)
+    instagram = serializers.CharField(max_length=30)
+    vk = serializers.CharField(max_length=30)
+    telegram = serializers.CharField(max_length=30)
+    record_type = serializers.CharField(max_length=7)
+    ads_category = serializers.CharField(max_length=20)
 
     def create(self, validated_data):
         return Record.objects.create(**validated_data)
